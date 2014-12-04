e332af61fc8aead1a5856c37509450c42a4da5c5
Matt Brown <matt.brown2@gmail.com>
2014-12-03 23:01:47 -0500

    Some more iterations on this to get a feel for nltk

---
+++ articles.py
@@ -0,0 +1,49 @@
+sample_article = '''
+GOP lawmakers, Benghazi survivors fume over House report
+A recent report by a GOP-led committee that was seen as going easy on the Obama administration's Benghazi response is drawing stinging complaints from a number of Republicans on the panel, as well as survivors of the attack.
+
+Some GOP members on the House Intelligence Committee grumble that the final product "might as well have been written by the minority," while other House Republicans say they are frustrated with the committee's decision to release a report with so many "holes."
+
+Several lawmakers point their fingers at the committee's chairman, Rep. Mike Rogers, R-Mich. Some members who disagreed with the findings said they stopped going to meetings because of concerns with his handling of the report, and didn't even take part in the final version.
+
+The report, released last month, found no intelligence lapses in connection with the fatal terror attack. Republican heavyweights like Sen. Lindsey Graham have blasted the findings as "garbage."
+
+But the findings also drew complaints from survivors, who testified behind closed doors, and caused deep divisions on the committee itself ahead of the report's release. Critical lawmakers wished to remain anonymous due to fear of blowback from Republican leadership.
+
+While House committee members have remained mostly silent, due to the secretive nature of the committee, some have complained to House Speaker John Boehner about the proceedings for months. Among the concerns was that Rogers was focusing strictly on the debate over the changed so-called "talking points" -- the administration's flawed narrative of the attack that initially blamed a protest over an anti-Islam film. Other Republicans on the committee, though, wanted the focus to be broader.
+
+Frustrations with Rogers have been boiling for more than a year, but nobody wanted to openly question the chairman with a midterm election looming, fearing it would give the media the chance to focus on Republican infighting rather than the issues.
+
+Even one Democrat on the committee said, "Rogers was more dismissive at times than Democrat members."
+
+No members would officially give consent to use their names, but all continue to question controversial maneuvers by Rogers and some members of his staff.
+
+"Rogers' staffers know more about some highly sensitive issues than some members. There is major stonewalling with information to other Republican members of the committee from Mike and some of his staffers," one House member who asked to go unnamed due to fear of backlash from GOP leadership told Fox News. "There is a lot of fighting and pressures behind the scenes and even some members oddly defending the Obama administration."
+
+Committee spokeswoman Susan Phalen said late Wednesday, "All members of HPSCI were given numerous opportunities to voice their views, comment on the report, or speak publicly or privately about the process. If any member disagreed with the process or outcome, every opportunity was made available for that member to voice his or her concerns."
+
+She added no member wrote to express any frustration.
+
+So why the internal battles? Why the struggle with members of the same party over a scandal that still has so many questions unanswered and an administration that still is vulnerable and unwilling to fully cooperate with the investigation?
+
+Now, at least five survivors contacted by Fox News say they too are frustrated and concerned their testimony is not fully represented. Kris Paronto and John Tiegan, both members of the CIA Annex Security Team who responded to the attack in Benghazi, say the report has major flaws.
+
+Paronto said: "Mike Rogers asked me ... 'Do you think that the delay cost lives?' ... and I looked him squarely in the eyes saying 'yes ... definitely.' We were told to wait and delayed three times which caused the deaths of Ambassador Stevens and Sean Smith. Why would the report say otherwise or our words be disregarded?"
+
+Paronto's concern is echoed by others on the ground that night in Benghazi. While official timelines have differed and stories from the State Department and administration changed, the story from the men and women on the ground has remained steadfast.
+
+"As I stated for the FBI, CIA and House intel committee, we were delayed, held back, told to stand down, however you want to put it," Paronto said. "We finally left without authorization. I only wish we had disobeyed orders sooner, because not only would we have rescued the five State Department security guys, we would have stopped the terrorists before they killed the ambassador and Sean Smith." Yet this account doesn't jibe with the report released by Rogers' committee last month.
+
+Tiegan adds, "We provided the same unchanging and accurate information of what took place in Benghazi on September 11, 2012 through September 12, 2012 to our immediate chain of command in the CIA's office of security, during several FBI interviews, and to Congressman Rogers and his committee. So it comes as a very big disappointment to us as those on the front lines that this report is full of inaccuracies and bias."
+
+Phelan, while calling the loss of American lives in Benghazi a "tragedy," added "ultimately the opinion of one witness about what could have or might have happened is not credible evidence."
+
+Their frustrations and concerns are echoed by members of the committee and others who have watched the hearings closely. Paronto, who along with Tiegen and others testified in front of the committee behind closed doors, said, "I think some people in Washington knew what was going on that evening in real time, and could not, or worse, would not make the tactically correct decisions ... and when Ambassador Stevens died, they wanted to cover their butts."
+
+The State Department and CIA weren't the only entities involved. Some operators in Benghazi on the night of the attack, who are still working for U.S. agencies, insist there were at least eight elected members of Congress read in on controversial operations that were ongoing in the country, including some Republicans. Operators also tell Fox News that many of these operatives and operations were completely unknown to the Libyan government and that would have caused an international incident if they were revealed. They wonder whether that is the reason some Republicans have allegedly been treading lightly.
+
+The frustrations with House intelligence and leadership also has extended to the new Benghazi Select Committee chaired by Rep. Trey Gowdy, R-S.C. Sources have said they were told to wait until after the midterm to truly go after information and even as they do, information leaks about witnesses, proceedings and details has hindered the process.
+
+As the investigation has picked up since the election, sources also say that the Gowdy-led commission has concerns about major pieces of information that the Rogers committee didn't take into consideration and important witnesses that were never called by the chairman. One survivor who has to remain anonymous because he still works on highly sensitive U.S. operations told Fox News, "I kept waiting for a phone call ... and nobody ever came."
+
+'''
---
+++ nlp_fun.py
@@ -0,0 +1,39 @@
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk import collocations
+from textblob import TextBlob
+import articles
+
+stops = [unicode(word) for word in stopwords.words('english')]
+
+
+def remove_stop_words(text):
+    meaningful_words = [sword.strip() for sword in text.split(' ') if sword.lower().strip() not in stops]
+    return meaningful_words
+
+
+def common_words(word_list):
+    return Counter(word_list).most_common()[:10]
+
+
+def find_bigrams(word_list):
+    bigram_measures = collocations.BigramAssocMeasures()
+    bigram_finder = collocations.BigramCollocationFinder.from_words(word_list)
+    print 'finding bigrams...'
+    # Filter to top 20 results; otherwise this will take a LONG time to analyze
+    bigram_finder.apply_freq_filter(20)
+    for bigram in bigram_finder.score_ngrams(bigram_measures.raw_freq)[:10]:
+        print bigram
+
+
+def score_feels(word_list):
+    return TextBlob(' '.join(word_list)).sentiment.polarity
+
+
+if __name__ == '__main__':
+    cleaned = remove_stop_words(articles.sample_article)
+    print common_words(cleaned)
+    #not very interesting, probably need to do this over an entire colleciton of documents using pandas or somehting?
+    find_bigrams(cleaned)
+
+    print score_feels(cleaned)
